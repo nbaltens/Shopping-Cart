69ad9bf058040611877ef96cc62bf4ba4feb2bce
Noah Baltensperger <nbalten@gmail.com>
2019-11-26 16:15:39 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
-/* Modal Window */
+/* Modal */
 
-// Get the modal
+
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("cartBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -139,7 +129,7 @@ function updateCartTotal() {
 
 
 
-/* responsive NAVBAR */
+/* NAVBAR */
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
